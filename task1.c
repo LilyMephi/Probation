d52b80f8704c03738cfa66a953db498fbc850a36
Lily <lilia200025@gmail.com>
2024-05-21 18:30:29 +0300

    first version

--- task1.c
+++ task1.c
@@ -1,9 +1,19 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define N 10
+#define N 16
+#define NTREADS 2
 
-void merge(int arr[], int left, int mid, int right) {
+//создаем массив который мудем сортировать
+int arr[N];
+
+typedef struct{
+	int left;
+	int right;
+} Range;
+
+// слияние двух массивов
+void merge(int left, int mid, int right) {
     int i, j, k;
     int n1 = mid - left + 1;
     int n2 = right - mid;
@@ -43,38 +53,58 @@ void merge(int arr[], int left, int mid, int right) {
     }
 }
 
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
-    }
+// функция которая будет сортировать слиянем в threads
+void * mergeSort(void * arg) {
+   Range *r = (Range *)arg;
+   int left = r->left;
+   int right = r->right;
+   int mid = left + (right -left)/2;
+   if( left < right){
+   	//разделяем массив на подмасивы пополам
+   	Range Rleft = {left, mid};
+   	Range Rright = {mid + 1, right};
+   	mergeSort(&Rleft);
+   	mergeSort(&Rright);
+   	
+   	merge(left,mid,right);
+   	
+   }
+   pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
+	pthread_t tid[NTREADS]; // идентификатор потока 
+	Range r[NTREADS]; // создаем для каждого потока свой ранг для массива
+	int size = N / NTREADS; // размер массива в каждом потоке
 
-	/*pthread_t tid; // идентификатор потока 
-	 pthread_attr_t attr; // отрибуты потока
-	
-	// получаем дефолтные значения атрибутов 
-  	pthread_attr_init(&attr);
-  	
-  	pthread_exit(0);*/
-  	srand(time(NULL));
-  	int arr[N];
+	// заполняем массив рандомными значениями
   	for(int i = 0; i < N; ++i){
   		arr[i] = rand()%100;
   		printf("%d ",arr[i]);
-  	}
+  	} 
   	printf("\n");
-  	mergeSort(arr, 0, N - 1);
+	
+	for(int i = 0; i < NTREADS; ++i){
+		r[i].left = i*size;
+		r[i].right = (i+1)*size - 1;
+		//создаем новый поток
+		if(pthread_create(&tid[i],NULL,mergeSort,&r[i]) != 0) { 
+			perror("Failed to create thread");
+		}
+	} 
+	for(int i = 0; i < N/2; ++i){
+		//Ждем окончание работы потока
+		if(pthread_join(tid[i],NULL) != 0) { perror("Failed to join thread");}
+		}
+	// соединаям отсортированные по потокам части массива
+	Range rf = {0, N-1};
+  	mergeSort(&rf);
+  	
+  	//выводим отсортированный массив
   	for(int i = 0; i < N; ++i){
   		printf("%d ",arr[i]);
   	}
   	printf("\n");
-  	//pthread_exit(0);
+  	return 0;
 }
